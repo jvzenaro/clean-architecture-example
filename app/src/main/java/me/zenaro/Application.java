413d8d50ed54cd7848ddcc2949a8b3474814442e
João Zenaro <joao@zenaro.me>
2021-08-26 23:52:04 -0300

    Criacao inicial da estrutura do projeto

---
+++ app/src/main/java/me/zenaro/Application.java
@@ -0,0 +1,11 @@
+package me.zenaro;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println(StringUtils.defaultString("Hello"));
+    }
+
+}
